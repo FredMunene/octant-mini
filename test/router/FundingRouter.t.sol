ff50e7338ca6590b7de83a85566fee50301201a6
fredmunene <mfredgitonga@gmail.com>
2025-11-08 02:55:03 +0300

    test: add router allocation fuzz

--- test/router/FundingRouter.t.sol
+++ test/router/FundingRouter.t.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import "forge-std/Test.sol";
 
 import {FundingRouter} from "../../src/router/FundingRouter.sol";
-import {MockERC20} from "../mocks/MockERC20.sol";
+import {MockERC20} from "../../src/mocks/MockERC20.sol";
 
 contract FundingRouterTest is Test {
     MockERC20 internal asset;
@@ -19,16 +19,10 @@ contract FundingRouterTest is Test {
 
     function testAddProgramAndRoute() public {
         FundingRouter.Program memory programA = FundingRouter.Program({
-            recipient: alice,
-            bps: 6_000,
-            metadataURI: "ipfs://a",
-            active: true
+            recipient: alice, bps: 6_000, metadataURI: "ipfs://a", active: true
         });
         FundingRouter.Program memory programB = FundingRouter.Program({
-            recipient: bob,
-            bps: 4_000,
-            metadataURI: "ipfs://b",
-            active: true
+            recipient: bob, bps: 4_000, metadataURI: "ipfs://b", active: true
         });
 
         router.addProgram(programA);
@@ -64,5 +58,33 @@ contract FundingRouterTest is Test {
         asset.mint(address(router), 100 ether);
         vm.expectRevert(FundingRouter.NoActivePrograms.selector);
         router.route();
+    }
+
+    function testFuzzSetAllocationsWithinBps(uint16 a, uint16 b, uint16 c) public {
+        router.addProgram(
+            FundingRouter.Program({recipient: alice, bps: 0, metadataURI: "a", active: true})
+        );
+        router.addProgram(
+            FundingRouter.Program({recipient: bob, bps: 0, metadataURI: "b", active: true})
+        );
+        router.addProgram(
+            FundingRouter.Program({
+                recipient: address(0xFEE), bps: 0, metadataURI: "c", active: true
+            })
+        );
+
+        uint256 total = uint256(a) + uint256(b) + uint256(c);
+        vm.assume(total <= router.BPS_DENOMINATOR());
+
+        uint16[] memory list = new uint16[](3);
+        list[0] = a;
+        list[1] = b;
+        list[2] = c;
+        router.setAllocations(list);
+
+        FundingRouter.Program[] memory configured = router.getPrograms();
+        assertEq(configured[0].bps, a);
+        assertEq(configured[1].bps, b);
+        assertEq(configured[2].bps, c);
     }
 }
