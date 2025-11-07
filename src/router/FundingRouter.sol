d907b9720424222851ca03bd281179f1ca93fd56
fredmunene <mfredgitonga@gmail.com>
2025-11-08 02:33:49 +0300

    feat: harden funding router routing math

--- src/router/FundingRouter.sol
+++ src/router/FundingRouter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.24;
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
-import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 
 /// @title FundingRouter
 /// @notice Splits harvested yield across configured public goods programs.
